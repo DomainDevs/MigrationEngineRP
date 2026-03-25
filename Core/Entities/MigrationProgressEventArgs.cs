beb05c4a038da0617d06ceb5c8ad91673f579f3b
Fernando <dario.fernando.poveda@gmail.com>
2026-03-24 23:06:40 -0500

    Clase

---
+++ Core/Entities/MigrationProgressEventArgs.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Entities
+{
+    public class MigrationProgressEventArgs : EventArgs
+    {
+        public double Progress { get; set; }
+        public string StepName { get; set; } = string.Empty;
+        public MigrationJob? Job { get; set; }
+    }
+}
