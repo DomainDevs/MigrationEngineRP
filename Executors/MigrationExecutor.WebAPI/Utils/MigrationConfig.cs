671903c63ee1367e2e37d3a9f2c964a0592b9ee0
Fernando <dario.fernando.poveda@gmail.com>
2026-03-28 12:54:05 -0500

    Conexión

--- Executors/MigrationExecutor.WebAPI/Utils/MigrationConfig.cs
+++ Executors/MigrationExecutor.WebAPI/Utils/MigrationConfig.cs
@@ -5,6 +5,8 @@ public class MigrationConfig
     public string NombreJob { get; set; } = string.Empty;
     public string CarpetaPaquetes { get; set; } = string.Empty;
     public string CarpetaLogs { get; set; } = string.Empty;
+    public string SourceDB { get; set; } = string.Empty;
+    public string DestinationDB { get; set; } = string.Empty;
     public List<string>? PaquetesIncluir { get; set; } = new();
     public List<string>? PaquetesOmitir { get; set; } = new();
 
